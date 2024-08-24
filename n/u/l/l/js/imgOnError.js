8a1820844bf5cbdca5bdea7206646110f0026a14
Music <gintonic13.6.music@gmail.com>
2024-08-24 15:04:25 +0700

    .

---
+++ n/u/l/l/js/imgOnError.js
@@ -0,0 +1,32 @@
+﻿function reloadImage(img) {
+    let baseUrl = "";
+    let arr = img?.src?.split("/n/u/l/l/");
+    if (arr?.length > 0) {
+        baseUrl = arr[arr?.length - 1];
+        if (baseUrl?.length > 0) {
+            let request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+            let arr = ['','2','3','4'];
+            for (let index = 0; index < arr?.length; index++) {
+                let host = arr[index];
+                let checkURl = "https://null-library"+host+".github.io/n/u/l/l/"+baseUrl;
+                try {
+                    request?.open('GET', checkURl, false);
+                    request?.send(); 
+                    if (request.status === 200) {
+                        let url = new URL(checkURl);
+                        url.searchParams.set('reload', 'true');
+                        img.src = url.toString();
+                        break;
+    
+                    }
+                } catch (error) {          
+                    console.log(error);
+                    
+                } 
+                
+            }
+        }
+    }    
+    img.onerror = null;
+
+}
